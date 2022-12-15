5ef3d5f90d50db2d17405028e0185a78ad9ba999
Dovavivavi <szilagyiabel1@gmail.com>
2022-12-15 18:15:23 +0100

    added basic structure of registration form

--- frontend/src/components/Registration/Registration.jsx
+++ frontend/src/components/Registration/Registration.jsx
@@ -3,7 +3,17 @@ import './Registration.scss'
 
 function Registration() {
   return (
-    <div>Registration</div>
+    <div id='form-section'>
+      <h1>Regisztráció</h1>
+      <div className='form-container'>
+        <form className='input-container'>
+          <input type='text' placeholder='Név' />
+          <input type='text' placeholder='Felhasználónév' />
+          <input type='password' placeholder='Jelszó' />
+          <button>Regisztráció</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
