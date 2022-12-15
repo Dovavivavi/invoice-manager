7d5deb3691f36d99c3ebb9e5e86ef14eac288584
Dovavivavi <szilagyiabel1@gmail.com>
2022-12-15 18:03:33 +0100

    added main components and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginHeader from './components/LoginHeader/LoginHeader';
+import LoginFooter from './components/LoginFooter/LoginFooter';
+import LoginForm from './components/LoginForm/LoginForm';
+import Registration from './components/Registration/Registration';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <LoginHeader />
+      <Routes>
+        <Route path='/' element={<LoginForm />} />
+        <Route path='/registration' element={<Registration />} />
+      </Routes>
+      <LoginFooter />
     </div>
   );
 }
---
+++ frontend/src/components/LoginFooter/LoginFooter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './LoginFooter.scss'
+
+function LoginFooter() {
+  return (
+    <div>LoginFooter</div>
+  )
+}
+
+export default LoginFooter
