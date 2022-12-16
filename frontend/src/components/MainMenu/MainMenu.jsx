075ca6b9e4aa7310971bc282faf65aa2d1084bc4
Dovavivavi <szilagyiabel1@gmail.com>
2022-12-16 11:01:05 +0100

    statechanges

--- frontend/src/components/MainMenu/MainMenu.jsx
+++ frontend/src/components/MainMenu/MainMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { signOut } from 'firebase/auth'
 import { Link, useNavigate } from 'react-router-dom'
 import { auth } from '../../firebase-config'
+import { onAuthStateChanged } from 'firebase/auth' 
 import './MainMenu.scss'
 
 function MainMenu() {
@@ -23,6 +24,17 @@ function MainMenu() {
   const current = new Date();
   const time = current.toLocaleTimeString('hu-HU')
 
+//--user data--
+  // auth.onAuthStateChanged((user) => {
+  //   if(user) {
+  //     console.log(user)
+  //   } else {
+  //     console.log('no user')
+  //   }
+  // })
+
+  auth && console.log(auth.currentUser)
+
   return (
     <>
       <div>MainMenu Header</div>
@@ -34,7 +46,7 @@ function MainMenu() {
       </div>
       <div>
         <h1>adatok:</h1>
-        <p>név</p>
+        <p>dipsnév</p>
         <p>felhasználónév</p>
         <p>jelszó</p>
         <p>{time}</p>
