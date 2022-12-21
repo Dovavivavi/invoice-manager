7d583a444c2bb53a6981d3b011566be96114a897
Dovavivavi <szilagyiabel1@gmail.com>
2022-12-21 08:58:06 +0100

    proxy and server set

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,20 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/captcha', async(req, res) => {
+  const {token} = req.body;
 
+  await fetch.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=6LfitIUjAAAAAJkdXsrrXV_FaAO3s3UBMnq9k-jO}&response=${token}`
+  )
+
+  if (res.status(200)) {
+    res.send('Test passed')
+  } else {
+    res.send('test failed')
+  }
+})
 
 app.listen(port, () => {
-  console.log(`the server is running on: https//127.0.0.1:${port}`)
+  console.log(`the server is running on: http://127.0.0.1:${port}`)
 });
